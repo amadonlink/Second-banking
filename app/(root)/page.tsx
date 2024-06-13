3665ad0809f268cd083e7abb23a52257538ae128
Amandi <amadonlinkserv@gmail.com>
2024-06-13 10:12:49 +0100

    Implement the complete layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import HeaderBox from '@/components/HeaderBox'
+import RightSidebar from '@/components/RightSidebar';
+import TotalBalanceBox from '@/components/TotalBalanceBox'
 
 const Home = () => {
+  const loggedIn = {firstName: 'Amandi', lastName: 'Nwankwo', email: 'amadonlinkserv@gmail.com'};
   return (
-    <div>Home</div>
+    <section className='home'>
+      <div className='home-content'>
+        <header className='home-header'>
+          <HeaderBox
+            type="greeting"
+            title="welcome"
+            subtext="Access and manage your account and transactions effeciently"
+            user={loggedIn?.firstName || 'Guest'}
+            />
+            <TotalBalanceBox 
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+            />
+        </header>
+          RECENT TRANSACTIONS
+      </div>
+
+      <RightSidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[ {currentBalance: 123.50}, {currentBalance: 234.30}]} 
+       />
+    </section>
   )
 }
 
